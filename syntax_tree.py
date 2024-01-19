2139bdc850de5e82e61ea2c2d0901d0dfe370852
Josh Frankel <joshmfrankel@gmail.com>
2024-01-19 17:51:56 -0500

    Add support for Sublime Text command palette

--- syntax_tree.py
+++ syntax_tree.py
@@ -5,10 +5,28 @@ import subprocess
 class SyntaxTreeOnSave(sublime_plugin.EventListener):
   def on_post_save_async(self, view):
     settings = sublime.load_settings("syntax_tree.sublime-settings")
-    format_on_save = settings.get("format_on_save", False)
+    stree_command_path = settings.get("stree_command_path")
 
-    if format_on_save == True and view.file_name().endswith('.rb'):
-      print("SyntaxTree: Running on save")
+    if not stree_command_path:
+      sublime.error_message("SyntaxTree: Please specify the path to your `stree` command with the `stree_command_path` setting.")
+    else:
+      format_on_save = settings.get("format_on_save", False)
 
-      stree_command_path = settings.get("stree_command_path")
+      if format_on_save == True and view.file_name().endswith('.rb'):
+        print("SyntaxTree: Running `write` on save.")
+
+        output = subprocess.check_output([stree_command_path, 'write', view.file_name()], stderr=subprocess.STDOUT, universal_newlines=True)
+
+class FormatSyntaxTreeCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    view = self.view
+    settings = sublime.load_settings("syntax_tree.sublime-settings")
+    stree_command_path = settings.get("stree_command_path")
+
+    if not stree_command_path:
+      sublime.error_message("SyntaxTree: Please specify the path to your `stree` command with the `stree_command_path` setting.")
+
+    if view.file_name().endswith('.rb'):
+      print("SyntaxTree: Running `write` on command.")
+
       output = subprocess.check_output([stree_command_path, 'write', view.file_name()], stderr=subprocess.STDOUT, universal_newlines=True)
